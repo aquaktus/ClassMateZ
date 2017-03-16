092bb5c0be0d765a9bb669459531f61241d12bae
AL-Hinai <2233129a@student.gla.ac.uk>
2017-03-16 22:31:56 +0000

    udate

--- ClassMateZ/urls.py
+++ ClassMateZ/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^login/$', views.user_login, name='login'),
     url(r'^register/$',views.register,name='register'),
+    url(r'^restricted/', views.restricted, name='restricted'),
 ]
