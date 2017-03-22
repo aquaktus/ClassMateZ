edb90f5fae17348cc6762cc4b655d58e3cf99e2d
aquaktus <aquaktus@yahoo.co.uk>
2017-03-22 18:18:51 +0000

    added new model structure

--- ClassMateZ/urls.py
+++ ClassMateZ/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
     url(r'^app/', include('app.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app/admin.py
+++ app/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from app.models import UserProfile
 from app.models import Class
 from app.models import Place
+from app.models import Layout
+from app.models import Zone
 
 # Register your models here.
 
 admin.site.register(UserProfile)
 admin.site.register(Class)
 admin.site.register(Place)
+admin.site.register(Layout)
+admin.site.register(Zone)
